a78cbf3cae7396aa9cef59ecb63da10f0b71d7b4
ernest_chaing <ernest_chaing@htc.com>
2017-02-24 15:49:03 +0800

    add two stage build script and update gcefuse using Go 1.8

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker build -t gcsfuse -f Dockerfile_gcsfuse .
+docker run -v $PWD/linux_amd64:/linux_amd64 gcsfuse  mv /go/bin/gcsfuse /linux_amd64
+docker build -t chiaen/docker-gcsfuse:go1.8 -f Dockerfile .
+docker push chiaen/docker-gcsfuse:go1.8
